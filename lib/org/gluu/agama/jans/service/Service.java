76010aeaaef83f0936cf45076568e665946870de
Mostafejur Rahman <mrraju.ice.iu@gmail.com>
2026-08-18 23:56:21 +0600

    refactor: update Service.java

--- lib/org/gluu/agama/jans/service/Service.java
+++ lib/org/gluu/agama/jans/service/Service.java
@@ -217,9 +217,11 @@ public class Service extends MagicLinkService{
         Object value = null;
         if (attribute.equals(this.UID)) {
             //user.getAttribute("uid", true, false) always returns null :(
-            value = user.getUserId();
+            // value = user.getUserId();
+            value = user.getUserName();
         } else {
-            value = user.getAttribute(attribute, true, false);
+            // value = user.getAttribute(attribute, true, false);
+            value = user.getAttribute("userName", true, false);
         }
         return value == null ? null : value.toString();
 
